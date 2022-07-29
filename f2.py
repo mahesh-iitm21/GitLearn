67f2865b6299cd9d493c5ed3c84c67c680b8fdf7
mahesh kumar <maheshnkumar782@gmail.com>
2022-07-30 00:17:21 +0530

    second commit

---
+++ f2.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 14:58:48 2022
+
+@author: mahes
+"""
+
+l="Monkeys write"
+m="New York times"
+a=""
+for i in l:
+    if i.isalpha()==True:
+        i=i.lower()
+        a=a+i
+print(a)  
+A=[]
+for i in range(len(a)):
+    A.append(a[i])
+b=""
+for j in m:
+    if j.isalpha()==True:
+        j=j.lower()
+        b=b+j
+print(b)  
+B=[]
+for i in b:
+    B.append(i)
+print(A)
+print(B)
+A.sort()
+B.sort()
+if A==B:
+    print("Hurray")
+
+      
